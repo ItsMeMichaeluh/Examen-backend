db8458b1612a9a0a2f99c4361411ce89a47210f2
uhJaimy <58993292+uhJaimy@users.noreply.github.com>
2025-06-03 15:03:35 +0200

    feat: Added Attendance, Log & Student tables

---
+++ migrations/Version20250603125554.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250603125554 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE attendance (id INT AUTO_INCREMENT NOT NULL, year INT NOT NULL, week INT NOT NULL, scheduled INT NOT NULL, logged INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE log (id INT AUTO_INCREMENT NOT NULL, mediaobject_id INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE student (id INT AUTO_INCREMENT NOT NULL, student_number VARCHAR(45) NOT NULL, active TINYINT(1) NOT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', updated_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            DROP TABLE attendance
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE log
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE student
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE `user`
+        SQL);
+    }
+}
---
+++ src/Entity/Attendance.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\AttendanceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AttendanceRepository::class)]
+#[ApiResource]
+class Attendance
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $year = null;
+
+    #[ORM\Column]
+    private ?int $week = null;
+
+    #[ORM\Column]
+    private ?int $scheduled = null;
+
+    #[ORM\Column]
+    private ?int $logged = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): static
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getWeek(): ?int
+    {
+        return $this->week;
+    }
+
+    public function setWeek(int $week): static
+    {
+        $this->week = $week;
+
+        return $this;
+    }
+
+    public function getScheduled(): ?int
+    {
+        return $this->scheduled;
+    }
+
+    public function setScheduled(int $scheduled): static
+    {
+        $this->scheduled = $scheduled;
+
+        return $this;
+    }
+
+    public function getLogged(): ?int
+    {
+        return $this->logged;
+    }
+
+    public function setLogged(int $logged): static
+    {
+        $this->logged = $logged;
+
+        return $this;
+    }
+}
